64470ae1ca37c4b42319c90ce709d47bb41c85f2
TuanSimonetti <54821029+TuanSimonetti@users.noreply.github.com>
2020-07-25 17:05:07 -0300

    Atualizacao no script, inserindo a funcao While e correcao da formula que converte C -> F

--- conversor_de_temperatura.py
+++ conversor_de_temperatura.py
@@ -1,10 +1,15 @@
-termometro = str(input("Qual a medida de temperatura deseja escolhar: Fahrenheit[F] ou Celsius[C]? "))
-temperatura = float(input("Qual a temperatura atual? "))
+continuar = "s"
 
-if termometro == "F":
-    conversao = (temperatura-32)/1.8
-    print(f"A medida em Fahrenheit é: {conversao}")
+while continuar == "s":
+    termometro = str(input("Para qual a medida de temperatura deseja converter: Fahrenheit[F] ou Celsius[C]? "))
+    temperatura = float(input("Qual a temperatura atual? "))
 
-if termometro == "C":
-    conversao = (1.8*temperatura)+32
-    print(f"A medida em Celsius é: {conversao}")
+    if termometro == "C" or termometro == "c" or termometro == "Celsius" or termometro == "celsius":
+        conversao = (temperatura - 32)/1.8
+        print(f"A medida em Fahrenheit é: {conversao}")
+
+    if termometro == "F" or termometro == "f" or termometro == "Fahrenheit" or termometro == "fahrenheit":
+        conversao = (((9/5) * temperatura) + 32)
+        print(f"A medida em Celsius é: {conversao}")
+
+    continuar = str(input("Deseja continar convertendo as temperaturas? "))
